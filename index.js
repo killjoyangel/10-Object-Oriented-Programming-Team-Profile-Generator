a8b71819bdd54e8a5e642a087b704d243bff0584
Ariel Marchand <marchand.aa@gmail.com>
2021-04-19 19:06:55 -0600

    test html page

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ const generateHTML = (answers) =>
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <!-- CSS only -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+        integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous" />
   <link rel="stylesheet" href="styles.css"/>
   <title>Programing Team Profile</title>
 </head>
 <body>
-
 <div class="jumbotron">
 <div class="p-3 mb-2 bg-danger text-white">
 <h1 class="display-4">My Team</h1>
