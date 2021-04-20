63cfdcec2126a92298ba5a7c9d2e2b86f659f52d
Ariel Marchand <marchand.aa@gmail.com>
2021-04-19 19:20:01 -0600

    did html skeleton deleted test index.html

--- index.js
+++ index.js
@@ -3,47 +3,72 @@ const fs = require('fs');
 
 const generateHTML = (answers) =>
   `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <!-- CSS only -->
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
-        integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous" />
-  <link rel="stylesheet" href="styles.css"/>
+  integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous" />
+  
   <title>Programing Team Profile</title>
-</head>
-<body>
-<div class="jumbotron">
-<div class="p-3 mb-2 bg-danger text-white">
-<h1 class="display-4">My Team</h1>
-</div>
-</div>
-
-
-
-
-
-
-
-
-
-
-
-/* <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4">Hi! My name is ${answers.name}</h1>
-    <p class="lead">I am from ${answers.location}.</p>
-    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-    <ul class="list-group">
-      <li class="list-group-item">My GitHub username is ${answers.github}</li>
-      <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
+  </head>
+  <body>
+  
+  <div class="jumbotron">
+  <div class="p-3 mb-2 bg-danger text-white">
+  <h1 class="display-4">My Team</h1>
+  </div>
+  </div>
+  
+  <div class="row row-cols-2 row-cols-md-3 g-5">
+    <div class="col">
+  <div id="manager" class="card" style="width: 18rem;">
+    <div class="p-3 mb-2 bg-primary text-white"><div class="card-header">
+  <p>${answers.name}</p>
+  <i class="fas fa-mug-hot"></i> ${answers.jobtitle}
+    </div>
+  </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">ID: ${answers.id}</li>
+      <li class="list-group-item"> Email: ${answers.email}</li>
+      <li class="list-group-item"> GitHub: ${answers.office}</li>
     </ul>
   </div>
-</div>
-</body>
-</html>`;
+  </div>
+  <div class="col">
+  <div id="engineer" class="card" style="width: 18rem;">
+    <div class="p-3 mb-2 bg-primary text-white"><div class="card-header">
+  <p>${answers.name}</p>
+  <i class="fas fa-glasses"></i> ${answers.jobtitle}
+    </div>
+  </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">ID: ${answers.id}</li>
+      <li class="list-group-item"> Email: ${answers.email}</li>
+      <li class="list-group-item"> GitHub: ${answers.office}</li>
+    </ul>
+  </div>
+  </div>
+  <div class="col">
+  <div id="student" class="card" style="width: 18rem;">
+    <div class="p-3 mb-2 bg-primary text-white"><div class="card-header">
+  <p>${answers.name}</p>
+  <i class="fas fa-user-graduate"></i> ${answers.student}
+    </div>
+  </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">ID: ${answers.id}</li>
+      <li class="list-group-item"> Email: ${answers.email}</li>
+      <li class="list-group-item"> School: ${answers.school}</li>
+    </ul>
+  </div>
+  </div>
+  </div>
+  </body>
+  </html>>`;
 
 inquirer
   .prompt([
