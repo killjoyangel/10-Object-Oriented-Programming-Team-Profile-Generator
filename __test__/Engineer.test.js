551ea4a49ee324b8b09eb2659129af0b6cb97eea
Ariel Marchand <marchand.aa@gmail.com>
2021-04-22 13:17:21 -0600

    eningineer test

--- __test__/Engineer.test.js
+++ __test__/Engineer.test.js
@@ -3,12 +3,12 @@ const { ecNormalize } = require("sshpk");
 const Engineer = require("../lib/Engineer");
 
 test("Create engineer", () => {
-  const engineer = new Engineer("name", "role", "id", "email", "github")
+  const engineer = new Engineer("name", role, "id", "email", "github")
   expect(engineer.getRole()).toBe('Engineer')
 })
 
 test("Github username", () => {
- const github = "killjoyangel2"
-  const engineer = new Engineer("name", "role", "id", "email", GitHub)
-  expect(engineer.getGitHub()).toBe(gitHub)
+ const gitHubName = "killjoyangel2"
+  const engineer = new Engineer("name", "role", "id", "email", gitHub)
+  expect(engineer.getGitHub).toBe(gitHub)
 })
